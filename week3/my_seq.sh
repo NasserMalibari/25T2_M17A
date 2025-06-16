59c85980ea35a9184f4b8a34226f1c59a42d0659
Nasser Malibari <nassermalibari1@gmail.com>
2025-06-16 18:29:41 +1000

    more complete seq example (more error checks)

--- week3/my_seq.sh
+++ week3/my_seq.sh
@@ -1,27 +1,56 @@
 #!/usr/bin/sh
 
-# '01' != '1'
-
-
 LAST="$1"
 FIRST=1
 INC=1
 
-if test "$#" -eq 2; then
-    # echo " i see 2 arguments"
-    # exit 0
+if test "$#" -eq 1; then
+    # FIRST="$1"
+    LAST="$1"
+elif test "$#" -eq 2; then
     FIRST="$1"
     LAST="$2"
+elif test "$#" -eq 3; then
+    FIRST="$1"
+    LAST="$3"
+    INC="$2"
+else
+    echo "some usage message (wrong num of args!)" >& 2
 fi
 
+if [ "$FIRST" -eq "$FIRST" ] 2> /dev/null; then
+    :
+else
+    echo "$0: Error <FIRST> must be an integer" >& 2
+    exit
+fi
 
+if [ "$INC" -eq "$INC" ] 2> /dev/null; then
+    if [ "$INC" -gt 0 ]; then
+        :
+    else
+        echo "$0: Error <INCREMENT> must be positive" >& 2
+        exit
+    fi
+else
+    echo "$0: Error <INCREMENT> must be an integer" >& 2
+    exit
+fi
 
-curr="$FIRST"
+if [ "$LAST" -eq "$LAST" ] 2> /dev/null; then
+    if [ "$LAST" -gt "$FIRST" ]; then
+        :
+    else
+        echo "$0: Error <LAST> must be greater than <FIRST>" >& 2
+        exit
+    fi
+else
+    echo "$0: Error <LAST> must be an integer" >& 2
+    exit
+fi
 
-# while curr <= last
-while [ $curr -le $LAST ]; do
-# while test "$curr" -le "$LAST"; do
-    echo "$curr"
-    curr=$(( curr + 1 ))
+CURRENT="$FIRST"
+while [ "$CURRENT" -le "$LAST" ]; do
+    echo "$CURRENT"
+    CURRENT=$(( CURRENT + INCREMENT ))
 done
-
